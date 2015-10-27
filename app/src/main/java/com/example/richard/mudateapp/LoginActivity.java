efd29f1d2ade1580389722b81497a8a574f4b548
Andrés Roberto <andresroberto@MacBook-de-Andres-Roberto-2.local>
2015-10-26 20:53:47 -0500

    Link login and signup with estates list view

--- app/src/main/java/com/example/richard/mudateapp/LoginActivity.java
+++ app/src/main/java/com/example/richard/mudateapp/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.richard.mudateapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso);
+    }
+
+    public void onClickHandler(View view){
+        String whereToGo = view.getTag().toString();
+        Intent intent = new Intent("com.example.richard.parcial." + whereToGo);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/richard/mudateapp/UsersActivity.java
+++ app/src/main/java/com/example/richard/mudateapp/UsersActivity.java
@@ -1,7 +1,9 @@
 package com.example.richard.mudateapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class UsersActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class UsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_users);
     }
 
+    public void onClickHandler(View view){
+        String whereToGo = view.getTag().toString();
+        Intent intent = new Intent("com.example.richard.parcial." + whereToGo);
+        startActivity(intent);
+    }
 }
